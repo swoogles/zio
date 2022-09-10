484c923e5ce2101aaf4d1ec1889b11f36042fa56
Bill Frasure <bill.frasure@gmail.com>
2022-09-09 22:02:47 -0600

    Define new ConsoleIO.LogLine type #time 5m

--- test/shared/src/main/scala/zio/test/ConsoleIO.scala
+++ test/shared/src/main/scala/zio/test/ConsoleIO.scala
@@ -1,7 +1,11 @@
 package zio.test
 
+import zio.test.ZTestLogger.LogEntry
+
 private[test] sealed trait ConsoleIO
 private[test] object ConsoleIO {
   case class Input(line: String)  extends ConsoleIO
   case class Output(line: String) extends ConsoleIO
+
+  case class LogLine(logLevel: LogEntry) extends ConsoleIO
 }
